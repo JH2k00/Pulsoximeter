40c60a97e73aebb27019b44d133f114333636bc2
EWiemer <elena-wiemer@hotmail.com>
2023-10-17 01:38:25 +0200

    Updated ble script. We can now receive data from the pico w over bluetooth on the PC

--- PC_Software/ble_client.py
+++ PC_Software/ble_client.py
@@ -1,9 +1,22 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
+import struct
 
-async def main():
-    devices = await BleakScanner.discover(timeout=20)
-    for d in devices:
-        print(d)
+name = "etv-pulse-oximeter"
 
-asyncio.run(main())
+def decode_heartrate(hr):
+    return struct.unpack(">h", hr)[0]
+
+async def main(name):
+    device = await BleakScanner.find_device_by_name(name)
+    if(device is None):
+        print("No Bluetooth device found with the name : %s" %name)
+        return
+    print(device)
+    async with BleakClient(device) as client:
+        heart_rate = 0
+        while(heart_rate is not None):
+            heart_rate = await client.read_gatt_char("00002a37-0000-1000-8000-00805f9b34fb")
+            print(decode_heartrate(heart_rate))
+
+asyncio.run(main(name))
